0ea1a1e2130523ae36da1d062d5b9530e0085b25
shehanp12 <shehanp068@gmail.com>
2020-10-25 23:58:54 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Hello(){
+  return <h1>I am Shehan Prasanna</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Hello/>
   );
 }
 
