1fe3ada8923d5c400930236915402ed202d4a0c6
shehanp12 <shehanp068@gmail.com>
2020-10-26 17:17:01 +0530

    Prop types

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,21 @@
 import  React,{Component} from  'react'
 import TodoItem from "./TodoItem";
+import PropTypes from 'prop-types'
+
 
 class  TodoApp extends Component{
     render(){
         return this.props.todos.map((todo) => (
-            <TodoItem todo={todo}/>
+            <TodoItem key={todo.id} todo={todo}/>
 
             ));
-
 
     }
 }
 
 export  default  TodoApp;
+
+//PropTypes
+TodoApp.propTypes = {
+    todo:PropTypes.array.isRequired
+}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,5 @@
 import  React,{Component} from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component{
 
@@ -10,8 +11,12 @@ class TodoItem extends Component{
 
         )
     }
+}
 
 
+//PropTypes
+TodoItem.propTypes= {
+    todo:PropTypes.object.isRequired
 }
 
 export  default  TodoItem;
